a1c41e75d94b38b5e584ba30d0759c881394fc62
Tharinduwi <dilshan.wijethunga99@gmail.com>
2025-10-24 15:16:00 +0530

    .

--- src/app/Model/Employee.ts
+++ src/app/Model/Employee.ts
@@ -6,17 +6,19 @@ export class EmployeeModel {
   state:string;
   emailId:string;
   contactNo:string;
-  address:string
+  address:string;
+  pinCode:string
 
 
   constructor() {
 
-    this.empId=0;
+    this.empId=1;
     this.name='';
     this.city='';
     this.state='';
     this.emailId='';
     this.contactNo='';
     this.address='';
+    this.pinCode='';
   }
 }
